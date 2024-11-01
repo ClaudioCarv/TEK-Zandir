ea56641152a96a284f5771f3179da79487ac9eb7
Cláudio Carvalho <96549956+ClaudioCarv@users.noreply.github.com>
2024-11-01 17:54:02 -0300

    Update SQL_queries.sql

--- Consultas SQL/SQL_queries.sql
+++ Consultas SQL/SQL_queries.sql
@@ -1,12 +1,12 @@
---consulta para retornar todos os clientes cadastrados
+--retornar todos os clientes cadastrados
 
 SELECT COUNT(*) AS todos_clientes FROM clientes;
 
---consulta para retornar a quantidade de clientes por cidade
+--retornar a quantidade de clientes por cidade
 
 SELECT cidade, COUNT(*) AS qntd_clientes FROM clientes GROUP BY cidade;
 
---consulta para retornar os 5 estados com mais clientes
+--retornar os 5 estados com mais clientes
 
 SELECT estado, COUNT(*) AS qntd_clientes
 FROM clientes
